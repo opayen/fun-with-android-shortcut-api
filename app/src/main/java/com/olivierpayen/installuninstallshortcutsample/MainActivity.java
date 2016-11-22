9fc559a05892558b0f6cbb2e824b6bd16156a37d
Olivier Payen <olivier.payen@truecaller.com>
2016-11-22 14:30:40 +0100

    Add button that installs the shortcut

--- app/src/main/java/com/olivierpayen/installuninstallshortcutsample/MainActivity.java
+++ app/src/main/java/com/olivierpayen/installuninstallshortcutsample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.olivierpayen.installuninstallshortcutsample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createShortcut(View view) {
+        Intent shortcutIntent = new Intent(getApplicationContext(), MainActivity.class);
+        shortcutIntent.setAction(Intent.ACTION_MAIN);
+
+        Intent addIntent = new Intent();
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "MyShortcut");
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, Intent.ShortcutIconResource.fromContext(getApplicationContext(), R.mipmap.ic_launcher));
+
+        addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+        addIntent.putExtra("duplicate", false);
+        getApplicationContext().sendBroadcast(addIntent);
     }
 }
